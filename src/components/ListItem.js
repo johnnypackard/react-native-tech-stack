c193db385f43b1d077a9d8c32523ce6a2d8f59f7
Johnny Packard <packard81@me.com>
2018-11-10 19:49:45 -0600

    refactor for cleanliness

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,11 +3,12 @@ import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
+
 class ListItem extends Component {
     renderDescription() {
-        const { library, selectedLibraryId } = this.props;
+        const { library, expanded } = this.props;
 
-        if (library.id === selectedLibraryId) {
+        if (expanded) {
             return (
                 <Text>{library.description}</Text>
             );
@@ -42,8 +43,10 @@ const styles = {
     }
 };
 
-const mapStateToProps = state => {
-    return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+
+    return { expanded };
 };
 
 export default connect(mapStateToProps, actions)(ListItem);
